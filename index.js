13db71812cb3fd31cf433487be9388e1c40e5a0b
James Correa <james.correa9090@gmail.com>
2021-11-07 10:54:22 -1000

    commit tree 98a9b3f984c33aef2918bf761294337c1d2dd8c1

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock) {
+    return Math.abs(42 - pickupBlock);
+}
+
+function distanceFromHqInFeet(pickupBlock) {
+    return distanceFromHqInBlocks(pickupBlock) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination)* 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+    if (distance < 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000){
+        return .02 * (distance - 400);
+    } else if ( distance > 2000 && distance < 2500){
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
